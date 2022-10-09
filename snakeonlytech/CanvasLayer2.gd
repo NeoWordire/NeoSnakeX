4470c6165fe21ec89fcd64705cf5ffff72f02fa9
NeoWordire <NeoWordire@gmail.com>
2022-10-09 16:16:12 -0700

    Improve gamefeel (control)
    fruit spawn in snake fix.

--- snakeonlytech/CanvasLayer2.gd
+++ snakeonlytech/CanvasLayer2.gd
@@ -13,6 +13,10 @@ export (Texture) var foodtex
 export (int) var FoodSegments = 5
 
 enum {NORTH, EAST, SOUTH, WEST, NODIR}
+const BIT1 = 1
+const BIT2 = 1 << 1
+const BIT3 = 1 << 2
+const BIT4 = 1 << 3
 
 # var a = 2
 # var b = "text"
@@ -30,21 +34,33 @@ var snakecap = 5
 var colmap = []
 var gameover = false
 var foodpoly = Polygon2D.new()
-
+var inputmask = 0;
+var oldinputmask = 0;
 var tempzero = []
 
 ## Called when the node enters the scene tree for the first time.
-func _ready():
+func reset():
 	rng.randomize()
 	get_node("GameOver").visible = false
+	for child in get_children():
+		if child.name != "GameOver":
+			child.queue_free()
+	playersprites = []
+	playertruecords = []
+	playertilerot = []
+#var playercolarea = []
+	colmap = []
 	for w in width:
 		for h in height:
 			colmap.append(0);
 	tempzero = colmap.duplicate()
-	#optional make player under root Node2D
-	#var node = Node2D.new()
-	#node.name = "Player"
-	#add_child(node)
+	snakelen = 0
+	snakecap = 5
+	gameover = false
+	foodpoly = Polygon2D.new()
+	dir = EAST
+	oldinputmask = 0
+	time = 0
 	foodpoly.polygon = PoolVector2Array([
 		Vector2(0,0),
 		Vector2(0,tilesize),
@@ -55,7 +71,24 @@ func _ready():
 	foodpoly.set_texture(foodtex)
 	add_child(foodpoly)
 	move_food()
-	pass # Replace with function body.
+
+func _ready():
+	reset()
+	#optional make player under root Node2D
+	#var node = Node2D.new()
+	#node.name = "Player"
+	#add_child(node)
+#	foodpoly.polygon = PoolVector2Array([
+#		Vector2(0,0),
+#		Vector2(0,tilesize),
+#		Vector2(tilesize,tilesize),
+#		Vector2(tilesize,0)
+#	])
+#	grow(playerstartpos, EAST)
+#	foodpoly.set_texture(foodtex)
+#	add_child(foodpoly)
+#	move_food()
+	#pass # Replace with function body.
 
 #call grow after moving but with old tail pos
 func grow(tailpos,rot):
@@ -96,10 +129,11 @@ func grow(tailpos,rot):
 func move_food():
 	foodpoly.position.x = rng.randi_range(0,width/tilesize - tilesize)*tilesize
 	foodpoly.position.y = rng.randi_range(0,height/tilesize- tilesize)*tilesize
-	if colmap[(foodpoly.position.y * height /tilesize) + (foodpoly.position.x/tilesize)] == 2:
+	if colmap[(foodpoly.position.y * height/tilesize) + (foodpoly.position.x/tilesize)] != 0:
 		move_food();
 #
 func game_over():
+	print("GAMEOVER")
 	gameover = true
 	var node = get_node("GameOver")
 	remove_child(node)
@@ -134,17 +168,26 @@ func _process(delta):
 	#if time < control_speed:
 	#	return;
 	#print(delta*100000)
-	
+
 	#print("sleep ", (1000/control_speed)-delta*1000, " plus delta =", delta*1000)
-	OS.delay_msec((1000/moves_per_second)-delta*1000)
-	get_input();
-	
-	if (dir == NODIR):
-		dir = EAST
-	#print(time)
-	time = 0
+	#time += delta
+	#if time < 1.0/moves_per_second:
+#		return;
 	if gameover:
 		return
+	#print(delta*100000)
+	
+	time = 0
+	get_input();
+	
+	#print(time)
+	
+	# HACK, REPLACE with clean update code, rough hack to make colmap accurate
+	colmap = tempzero.duplicate()
+	for i in playertruecords.size():
+		colmap[playertruecords[i].y*height/tilesize + playertruecords[i].x/tilesize] = 1
+	colmap[(foodpoly.position.y * height /tilesize) + (foodpoly.position.x/tilesize)] = 2
+	
 	var oldtailcord = playertruecords[-1]
 	
 	#Move Head,
@@ -160,51 +203,48 @@ func _process(delta):
 		print("FOOD")
 		move_food()
 		snakecap += FoodSegments
-	
 	if (snakelen< snakecap):
 		grow(oldtailcord, dir)
-#Follow Head
+	#Follow Head
 	for i in snakelen-1:
 		var inverse = snakelen - i - 1
 		playertruecords[inverse] = playertruecords[inverse-1] 
 		playersprites[inverse].position = playersprites[inverse - 1].position
 		playersprites[inverse].rotation = playersprites[inverse - 1].rotation
-
-	# HACK, REPLACE with clean update code, rough hack to make colmap accurate
-	colmap = tempzero.duplicate()
-	for i in playertruecords.size():
-		colmap[playertruecords[i].y*height/tilesize + playertruecords[i].x/tilesize] = 1
-	colmap[(foodpoly.position.y * height /tilesize) + (foodpoly.position.x/tilesize)] = 2
+	OS.delay_msec((1000/moves_per_second)-delta*1000)
 	
 func get_input():
 	# velocity = Vector2()
+	inputmask = 0;
 	if Input.is_action_pressed("ui_right"):
-		if (dir != WEST) && (dir != EAST):
-		  dir = EAST
+		inputmask |= BIT1
 	if Input.is_action_pressed("ui_left"):
-		if (dir != WEST) && (dir != EAST):
-		  dir = WEST
+		inputmask |= BIT2
 	if Input.is_action_pressed("ui_down"):
-		if (dir != NORTH) && (dir != SOUTH):
-		  dir = SOUTH
+		inputmask |= BIT3
 	if Input.is_action_pressed("ui_up"):
-		if (dir != NORTH) && (dir != SOUTH):
-		  dir = NORTH
-	#velocity = velocity.normalized() * speed
+		inputmask |= BIT4
+	if inputmask != oldinputmask:
+		if (inputmask & (~oldinputmask)) == BIT1 && dir != WEST:
+			dir = EAST
+		elif (inputmask & (~oldinputmask)) == BIT2 && dir != EAST:
+			dir = WEST
+		elif (inputmask & (~oldinputmask)) == BIT3 && dir != NORTH:
+			dir = SOUTH
+		elif (inputmask & (~oldinputmask)) == BIT4 && dir != SOUTH:
+			dir = NORTH
+		else :
+			oldinputmask = 0
+			if Input.is_action_pressed("ui_right") &&  dir != WEST:
+				dir = EAST
+			if Input.is_action_pressed("ui_left") &&  dir != EAST:
+				dir = WEST
+			if Input.is_action_pressed("ui_up") &&  dir != SOUTH:
+				dir = NORTH
+			if Input.is_action_pressed("ui_down") &&  dir != NORTH:
+				dir = SOUTH
+	oldinputmask = inputmask
 
 func _on_GameOver_pressed():
-	for child in get_children():
-		if child.name != "GameOver":
-			child.queue_free()
-	playersprites = []
-	playertruecords = []
-	playertilerot = []
-#var playercolarea = []
-	snakelen = 0
-	snakecap = 5
-	colmap = []
-	gameover = false
-	foodpoly = Polygon2D.new()
-	dir = EAST
-	_ready()
+	reset()
 	pass # Replace with function body.
