18d519226c0f084011290b331bc2eb9ab7c2a8a1
NeoWordire <NeoWordire@gmail.com>
2022-10-22 15:24:11 -0700

    WENT HAM FLESHING OUT BATTLE IDEAS

--- snakeonlytech/Scripts/MainSnake.gd
+++ snakeonlytech/Scripts/MainSnake.gd
@@ -9,10 +9,14 @@ export (int) var snake_mps = 8
 export (int) var FoodSegments = 1
 export (bool) var debugging = false
 export (float) var ShootCooldown = 0.5 
+export (float) var timer = 30.0
+var battleState = 0 # 0 == PRESTART, 1 == IN BATTLE, 2 GAMEOVER LOST, 3 GAMEOVERWIN
 
+
+var timerinstance
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("BGM").stream_paused = true
+	#get_node("BGM").stream_paused = true
 	GlobalSnakeVar.g_bullet_moves_per_second = bullet_mps
 	GlobalSnakeVar.g_snake_moves_per_second = snake_mps
 	GlobalSnakeVar.g_FoodSegments = FoodSegments
@@ -21,11 +25,18 @@ func _ready():
 	GlobalSnakeVar.debug = debugging
 	GlobalSnakeVar.g_foodsegments = FoodSegments
 	GlobalSnakeVar.g_rng.randomize()
-	reset()
+	#beginFight()
+	GlobalSnakeVar.paused = true
+	pre_start()
 	pass # Replace with function body.
 
-func reset():
-	get_node("GameOver").visible = false
+func start():
+	GlobalSnakeVar.paused = false
+
+func beginFight():
+	timerinstance = timer
+	get_node("PRESTART").visible = false
+	battleState = 1
 	GlobalSnakeVar.paused = false
 	GlobalSnakeVar.initColMap()
 	if (GlobalSnakeVar.snakes.empty()):
@@ -50,6 +61,16 @@ func reset():
 func _process(_delta):
 	if Input.is_action_just_released("ui_end"):
 		GlobalSnakeVar.debug = !GlobalSnakeVar.debug
+	if Input.is_action_just_released("ui_accept"):
+		if battleState == 0:
+			startRequested()
+		if battleState == 2:
+			pre_start()
+		if battleState == 3:
+			breakpoint # NEXT SCENE
+	if (!GlobalSnakeVar.paused):
+		get_node("HUD").get_node("CountdownTimer").text = String(ceil(timerinstance))
+		timerinstance -= _delta
 
 var bulletlastrun = 0
 var counterlastsnake = 0
@@ -58,6 +79,10 @@ func _physics_process(delta):
 	GlobalSnakeVar.snakeupdatetimer += delta
 	GlobalSnakeVar.bulletupdatetimer += delta
 	if GlobalSnakeVar.paused:
+		return
+	if (timerinstance <= 0.0):
+		timerinstance = 0
+		timer_complete()
 		return
 		#if (updatetime*GlobalSnakeVar.moves_per_second >= 1.0):
 	if (GlobalSnakeVar.snakeupdatetimer * GlobalSnakeVar.g_snake_moves_per_second >= 1.0):
@@ -83,20 +108,40 @@ func _physics_process(delta):
 		GlobalSnakeVar.bulletupdatetimer = 0
 
 func _on_GameOver_pressed():
-	reset()
+	beginFight()
+	
+func startRequested():	
+	beginFight()
 
+func timer_complete():
+	if (GlobalSnakeVar.snakes[0].sprites.size() < GlobalSnakeVar.snakes[1].sprites.size()):
+		game_over_lost("YOUR SNAKE SMALL")
+	battleState = 3
+	get_node("WINSCREEN").visible = true
+
+func pre_start():
+	battleState = 0
+	get_node("LOSESCREEN").visible = false
+	get_node("WINSCREEN").visible = false
+	get_node("PRESTART").visible = true
+	get_node("PRESTART").raise()
+	
+func game_over_lost(reason):
+	battleState = 2
+	print("LOST BECAUSE =", reason)
+	GlobalSnakeVar.paused = true
+	get_node("LOSESCREEN").visible = true
+	get_node("WINSCREEN").visible = false
+	get_node("LOSESCREEN/REASON").text = "GAMEOVER \n " + reason
+	get_node("LOSESCREEN").raise()
+	
 func _on_Player_snake_died(player):
 	SoundPlayer.play_sound(SoundPlayer.SFXSNAKEDEFEATED)
 	print("player = ", player, "has DIED")
-	get_node("GameOver").visible = true
-	get_node("GameOver").raise()
-	GlobalSnakeVar.paused = true
-	
+	game_over_lost("YOU CRASHED")
 
 func _on_Enemy_snake_died(player):
 	SoundPlayer.play_sound(SoundPlayer.SFXSNAKEDEFEATED)
 	print("player = ", player, "has DIED")
-	get_node("GameOver").visible = true
-	get_node("GameOver").raise()
-	GlobalSnakeVar.paused = true
+	game_over_lost("WTF ENEMY DIED????????????????????????/")
 	pass
